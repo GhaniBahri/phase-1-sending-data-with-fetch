2f965bb87f275f34c8547a8c1e93aeab3b27e4d8
GhaniBahri <bahri.ghanou@gmail.com>
2023-09-01 00:05:24 +0200

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+
+    },
+    body: JSON.stringify({
+        dogName: "Byron",
+        dogBreed: "Poodle",
+      }),
+  };
+  function submitData(name, email){
+    const user= {name, email}
+    const headers= {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(user)
+    }
+    return fetch('http://localhost:3000/users', headers).then(res=>res.json()).then(data=>document.body.append(data.id))
+            .catch(err=>document.body.append( err.message))
+  }
